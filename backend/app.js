6720a13d9a67e36704ea073425cdb6351a498c3d
tejaswiniaher <tejaswiniaher905@gmail.com>
2026-03-13 16:02:55 +0530

    fix express wildcard route for express 5

--- backend/app.js
+++ backend/app.js
@@ -52,7 +52,7 @@ if (process.env.NODE_ENV === "PRODUCTION") {
 
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/.*/", (req, res) => {
+    app.use((req, res) => {
         res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
 }
