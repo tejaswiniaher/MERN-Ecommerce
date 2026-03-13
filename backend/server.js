557c8ac9b83aab12d79dd82f78bbe6e28b7a2d3f
tejaswiniaher <tejaswiniaher905@gmail.com>
2026-03-13 16:25:06 +0530

    fix env loading

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
-import app from './app.js';
+
 import dotenv from 'dotenv';
+dotenv.config({path:'backend/config/config.env'});
+import app from './app.js';
 import { connectMongoDatabase } from './config/db.js';
-if(process.env.NODE_ENV!=='PRODUCTION'){
-dotenv.config({path:'backend/config/config.env'})
-}
 import {v2 as cloudinary} from 'cloudinary';
-import Razorpay from 'razorpay';    
-connectMongoDatabase();
-cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_NAME,
-    api_key:process.env.API_KEY,
-    api_secret:process.env.API_SECRET
-})
+import Razorpay from 'razorpay'; 
+
 // Handling uncaught exceptions errors
 process.on("uncaughtException", (err)=>{
     console.log(`Error: ${err.message}`);
     console.log(`Shutting down the server due to uncaught exception`);     
     process.exit(1);
 })  
-const port = process.env.PORT || 3000;
+
+connectMongoDatabase();
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.API_KEY,
+    api_secret:process.env.API_SECRET
+})
+
+
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_API_SECRET
 })
+
+const port = process.env.PORT || 8000;
 
 const server = app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
